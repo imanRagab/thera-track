32863ffabf1a08a16ac04fcedb1b108c3dae3090
Andrey Masunov <andrey.masunov@dci-student.org>
2024-12-04 03:23:20 +0100

    Made changes for patient page

--- src/main/java/org/dci/theratrack/entity/Patient.java
+++ src/main/java/org/dci/theratrack/entity/Patient.java
@@ -2,6 +2,7 @@ package org.dci.theratrack.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -28,6 +29,7 @@ import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
@@ -36,7 +38,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "patients",
     uniqueConstraints = @UniqueConstraint(columnNames = {"email", "phone"}))
 //ensures that the combination of email and phone is unique
-
+@ToString(exclude = {"therapists","diagnoses", "appointment", "user"})
 public class Patient {
 
   @Id
@@ -67,6 +69,7 @@ public class Patient {
 
   @Past // Ensures the birthDate is a past date
   @Column(nullable = false)
+  @JsonProperty("birthdate")
   private LocalDate birthDate;
 
   @Size(max = 500)
@@ -94,7 +97,6 @@ public class Patient {
   @JoinColumn(name = "user_id", unique = true)
   @JsonBackReference
   private User user;
-
 
 
   public User getUser() {
